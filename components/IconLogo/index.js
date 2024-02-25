06874b1fbd06b9ffa62539f1d40d2bc5b619f600
akmaljon <akmaljon@gmail.com>
2024-02-25 15:53:36 +0500

    changed

--- components/IconLogo/index.js
+++
@@ -1,6 +0,0 @@
-import { InstagramLogo } from "./Instagram";
-import { TiktokLogo } from "./Tiktok";
-import { TwitterLogo } from "./Twitter";
-import {HamburgerMenu} from "./HamburgerMenu"
-
-export {InstagramLogo, TiktokLogo, TwitterLogo, HamburgerMenu}
