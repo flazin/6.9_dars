c7c4c73a1a65b7b8ac5dbd4e202a17d1f2f644df
akmaljon <akmaljon@gmail.com>
2024-02-25 13:45:59 +0500

    aS

--- src/data/articles.js
+++ src/data/articles.js
@@ -3,10 +3,15 @@ import React from "react";
 function article_1() {
 	return {
 		date: "7 May 2023",
-		title: "Hi      ,I’m Charles, Front-end Developer",
+		title: "The Benefits of Cloud Computing",
 		description:
-			"I design and develop experiences that make people’s lives simpler through Web and Mobile apps.I work with FIgma , HTML5, CSS3, JavaScript, React, ReactNative and Flutter.",
-
+			"Cloud computing offers a range of benefits, including cost savings and increased flexibility. Find out why more businesses are turning to the cloud.",
+		keywords: [
+			"The Benefits of Cloud Computing",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
 		style: `
 				.article-content {
 					display: flex;
@@ -23,7 +28,11 @@ function article_1() {
 			<React.Fragment>
 				<div className="article-content">
 					<div className="paragraph">Content of article 1</div>
-					<img src="" alt="random" className="randImage" />
+					<img
+						src="https://picsum.photos/200/300"
+						alt="random"
+						className="randImage"
+					/>
 				</div>
 			</React.Fragment>
 		),
@@ -32,12 +41,17 @@ function article_1() {
 
 function article_2() {
 	return {
-		date: "",
-		title: "Hi      ,I’m Charles, Front-end Developer",
+		date: "7 May 2023",
+		title: "Artificial Intelligence in Healthcare",
 		description:
-			"I design and develop experiences that make people’s lives simpler through Web and Mobile apps.I work with FIgma , HTML5, CSS3, JavaScript, React, ReactNative and Flutter.",
+			"AI is transforming the healthcare industry, from improving patient outcomes to streamlining operations. Discover the latest applications of this game-changing technology.",
 		style: ``,
-
+		keywords: [
+			"Artificial Intelligence in Healthcare",
+			"Tharindu",
+			"Tharindu N",
+			"Tharindu Nayanajith",
+		],
 		body: (
 			<React.Fragment>
 				<h1>Content of article 2</h1>
